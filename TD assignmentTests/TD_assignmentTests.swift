73bc987ec4155e8785b0349cd45f07c49def60b9
codeblue009 <codeblue009@gmaill.com>
2018-10-20 19:17:43 -0400

    Initial Commit

---
+++ TD assignmentTests/TD_assignmentTests.swift
@@ -0,0 +1,36 @@
+//
+//  TD_assignmentTests.swift
+//  TD assignmentTests
+//
+//  Created by Sacha Sukhdeo on 2018-10-20.
+//  Copyright © 2018 Sacha Sukhdeo. All rights reserved.
+//
+
+import XCTest
+@testable import TD_assignment
+
+class TD_assignmentTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
