7855439bd4ec59caf8b776293fe97053ffa4283d
codeblue009 <codeblue009@gmaill.com>
2018-10-21 18:15:41 -0400

    installed Alamofire and SwiftyJSON cocoapods

---
+++ Podfile
@@ -0,0 +1,22 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'TD assignment' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for TD assignment
+    pod 'SwiftyJSON'
+    pod 'Alamofire', '~> 4.7'
+
+  target 'TD assignmentTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'TD assignmentUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
--- TD assignment/TopicsListVC.swift
+++ TD assignment/TopicsListVC.swift
@@ -1,25 +1,23 @@
-//
 //  ViewController.swift
 //  TD assignment
 //
 //  Created by Sacha Sukhdeo on 2018-10-20.
 //  Copyright © 2018 Sacha Sukhdeo. All rights reserved.
-//
 
 import UIKit
 
-class ViewController: UIViewController {
+class TopicsListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
+extension TopicsListVC: UITableViewDelegate {
+    
+}
