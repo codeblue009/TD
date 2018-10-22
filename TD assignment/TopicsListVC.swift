d96271258c5dddbc3df90284e16af93f63997a69
codeblue009 <codeblue009@gmaill.com>
2018-10-22 00:32:20 -0400

    Add UITableView and NavigationController to storyboard. Add UITableView delegate and datasource methods to controller.

--- TD assignment/TopicsListVC.swift
+++ TD assignment/TopicsListVC.swift
@@ -5,11 +5,26 @@
 //  Copyright © 2018 Sacha Sukhdeo. All rights reserved.
 
 import UIKit
+import SwiftyJSON
 
 class TopicsListVC: UIViewController {
 
+    var topics: [JSON]?
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        TopicsListData().getTopics(completion: { json in
+            self.topics = json
+            self.tableView.reloadData()
+            print(self.topics)
+        })
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -18,6 +33,34 @@ class TopicsListVC: UIViewController {
 
 }
 
-extension TopicsListVC: UITableViewDelegate {
+extension TopicsListVC: UITableViewDataSource, UITableViewDelegate {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.topics?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return false
+    }
+    
+    func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+    }
+    
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        if let topics = topics,
+            topics.count > indexPath.row {
+            let topic = topics[indexPath.row]
+            cell.textLabel?.text = topic["Text"].string
+        }
+    }
     
 }
